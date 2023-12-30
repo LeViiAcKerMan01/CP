3a359e58c79816cc3d9d65ae8ce9fa66bb95a415
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-12-30 18:52:27 +0530

    First commit

---
+++ strRev.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a;
+    int i,n;
+
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+
+    }
+    for(int i=(n-1); i>=0; i--){
+        cout<<a[i];
+    }
+    return 0;
+}
+//Easy implementation of string reversal in CPP
