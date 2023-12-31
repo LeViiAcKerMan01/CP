fc5ae0702b73b69e78d888fc6c1b0c54666c85e1
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-12-31 18:43:11 +0530

    First commit

---
+++ vectorbt.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+string 
+int main(){
+    string a;
+    int i,n;
+    cin>>a;
+    n=a.size();
+    for(int i=(n-1); i>=0; i--){
+        if(i==" ")
+        cout<<a[i];
+        
+        
+    }
+    return 0;
+    
+    
+}
