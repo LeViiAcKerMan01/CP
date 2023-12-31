2831693edeb169153dc61e430134049ee3adfe0e
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2024-01-01 01:27:44 +0530

    First commit

---
+++ vect.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    int val=0; 
+    for(int i=0; i<10; i++){
+        
+        v.push_back(i);
+        
+    }
+    for(int i=9; i>=0; i--){
+        cout<<v[i]<<" ";
+
+    }
+    return 0;
+}
