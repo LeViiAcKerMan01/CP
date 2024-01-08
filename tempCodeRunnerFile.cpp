a9c346ea2048657a9755085e81310b0941d4704f
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-01-08 09:51:51 +0530

    First commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    #ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
 int a;
 int b;
-cout<<"Enter no.:<<endl;
+int test;
+cin>>test;
+for(int i=0; i<test; i++){
+cout<<"Enter no.:";
 cin>>a>>b;
+cout<<"Number entered is :"<<a<<" " <<b<<" ";
+
 int c=pow(a,b);
-cout<<c;
+cout<<c<<"\n";
+}
+
+return 0;
+}
